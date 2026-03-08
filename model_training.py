ac4e116e57d74bdebf70b39c9f1ee06086e9abe8
SagarpatilSP <115875563+SagarpatilSP@users.noreply.github.com>
2026-03-08 16:07:24 +0530

    Refactor model training script for S3 bucket usage

--- model_training.py
+++ model_training.py
@@ -1,9 +1,10 @@
+import os
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import RandomForestRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
-import os
-from dotenv import load_dotenv
+
 load_dotenv()
 
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
@@ -12,21 +13,14 @@ BUCKET = os.getenv("BUCKET_NAME")
 
 spark = SparkSession.builder \
     .appName("Customer Sales ML Model") \
-    .config("spark.hadoop.fs.s3a.access.key", AWS_ACCESS_KEY)\
-    .config("spark.hadoop.fs.s3a.secret.key", AWS_SECRET_KEY)\
+    .config("spark.hadoop.fs.s3a.access.key", AWS_ACCESS_KEY) \
+    .config("spark.hadoop.fs.s3a.secret.key", AWS_SECRET_KEY) \
+    .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
     .getOrCreate()
 
+df = spark.read.parquet(f"s3a://{BUCKET}/gold/customer_sales")
 
-# Load Gold Dataset
-df = spark.read.parquet(
-    "s3a://my-data-lake/gold/customer_sales"
-)
-
-# Feature Columns
-feature_cols = [
-    "total_customer_sales",
-    "avg_customer_sales"
-]
+feature_cols = ["avg_customer_sales"]
 
 assembler = VectorAssembler(
     inputCols=feature_cols,
@@ -35,43 +29,28 @@ assembler = VectorAssembler(
 
 ml_df = assembler.transform(df)
 
-# Train Test Split
 train_df, test_df = ml_df.randomSplit([0.8, 0.2], seed=42)
 
-# Model
 rf = RandomForestRegressor(
     featuresCol="features",
     labelCol="total_customer_sales",
-    numTrees=100,
-    maxDepth=10
+    numTrees=100
 )
 
 model = rf.fit(train_df)
 
-# Prediction
 predictions = model.transform(test_df)
 
-predictions.select(
-    "Customer_Id",
-    "total_customer_sales",
-    "prediction"
-).show()
-
-
-# Evaluation
 evaluator = RegressionEvaluator(
     labelCol="total_customer_sales",
     predictionCol="prediction",
     metricName="rmse"
 )
 
-rmse = evaluator.evaluate(predictions)
+print("RMSE:", evaluator.evaluate(predictions))
 
-print("Model RMSE:", rmse)
-
-# Save Model
 model.write().overwrite().save(
-    "s3a://my-data-lake/models/customer_sales_model"
+    f"s3a://{BUCKET}/models/customer_sales_model"
 )
 
 spark.stop()
